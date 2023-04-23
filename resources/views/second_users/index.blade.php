2dadf924b14f9538aee255e39ee71ec5f96025bb
Hiren <hirenkeradiya@gmail.com>
2023-04-23 14:43:16 +0530

    Remove display users

--- resources/views/second_users/index.blade.php
+++ resources/views/second_users/index.blade.php
@@ -9,50 +9,7 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
             <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
                 <div class="relative overflow-x-auto">
-                    <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
-                        <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-                            <tr>
-                                <th scope="col" class="px-6 py-3">
-                                    Name
-                                </th>
-                                <th scope="col" class="px-6 py-3">
-                                    Email
-                                </th>
-                                <th scope="col" class="px-6 py-3">
-                                    Mobile Number
-                                </th>
-                                <th scope="col" class="px-6 py-3">
-                                    Action
-                                </th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            @foreach ($secondUsers as $secondUser)
-                                <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-                                    <th scope="row"
-                                        class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                                        {{ $secondUser->name }}
-                                    </th>
-
-                                    <td class="px-6 py-4">
-                                        {{ $secondUser->email }}
-                                    </td>
-
-                                    <td class="px-6 py-4">
-                                        {{ $secondUser->mobile_number }}
-                                    </td>
-
-                                    <td class="px-6 py-4">
-                                        Edit | Delete
-                                    </td>
-                                </tr>
-                            @endforeach
-                        </tbody>
-                    </table>
-
-                    <div class="mt-5">
-                        {{ $secondUsers->links() }}
-                    </div>
+                    Welcome {{ Auth::user()->name }}
                 </div>
             </div>
         </div>
